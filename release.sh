7cc1e09a3a861a7e7ff93a5f871be57a6f02a3c0
oleksandr.koval <oleksandr.koval@stripo.email>
2024-02-14 17:10:26 +0200

    fix repo url

--- release.sh
+++ release.sh
@@ -8,4 +8,5 @@ echo "helm package"
 helm package helm-chart-sources/* 
 
 echo "helm index"
-helm repo index --url https://stripoinc.github.io/stripo-plugins-charts/ .
+#helm repo index --url https://stripoinc.github.io/stripo-plugins-charts/ .
+helm repo index --url https://raw.githubusercontent.com/stripoinc/stripo-plugins-charts/main/ .
